44f558a559f717123ca3d67cbcd14f87249ce99c
gabor-farkas <gabor.farkas@doctusoft.com>
2016-06-20 21:31:27 +0200

    added the table to the exports

--- dist/aurelia-dynamic-parts.d.ts
+++ dist/aurelia-dynamic-parts.d.ts
@@ -1,4 +1,6 @@
 declare module 'aurelia-dynamic-parts' {
     export * from 'dynamic-panel/dynamic-panel';
     export * from 'dynamic-panel/dynamic-panel-builder';
+    export * from 'dynamic-table/dynamic-table';
+    export * from 'dynamic-table/dynamic-table-builder';
 }
